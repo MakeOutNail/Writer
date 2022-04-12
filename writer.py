797fac1c41cd992e640cea7c7e7256ebc16821ef
User <User@pc.com>
2022-04-12 10:09:29 +0200

    Not finished yet!

--- writer.py
+++ writer.py
@@ -8,13 +8,52 @@ class main:
     def __init__(self):
         self.keyboard = pynput.keyboard.Controller()
         self.text = "Hello World!"
+        self.message = "Good day!"
         self.time = 0
+        self.save = []
+        self.config = {
+            "text": "Hello World!",
+            "message": "Good day!",
+            "time": "0"
+        }
+
+        try:
+            with open('config.json') as i:
+                self.save = json.load(i)
+        except:
+            with open('config.json', 'w') as i:
+                print(i)
+                json.dump(self.config, i)
+
+        self.text = self.save['text']
+        self.text = self.save['text']
+        self.text = self.save['text']
+
+    def settings(self):
+        option = input("Options/")
+
+        if(option == 'm' or option == 'M'):
+            self.message = input("Options/Message/")
+            self.settings()
+        elif(option == 'Q' or option == 'q'):
+            self.main()
 
     def write(self):
         time.sleep(self.time)
         self.keyboard.type(self.text)
 
     def main(self):
+
+        print("")
+        print(self.message)
+        print("")
+
+        print("Do you want to go into setting mode?[y]")
+        settings = input(":")
+
+        if(settings == 'y' or settings == 'Y'):
+            self.settings()
+
         print("****************************************")
         self.text = input("Text:")
         print("****************************************")
